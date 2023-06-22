496f97141603d8e149e4336512546f47bc0016eb
Marco Polello <mpolello@alloy.it>
2023-06-22 14:53:19 +0200

    fix env import

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import RandNFT from '../artifacts/contracts/RandNFT.sol/RandNFT.json';
 import dotenv from 'dotenv';
 
 
-dotenv.config();
+const process = dotenv.config();
 
 const PINATA_CONTENT = process.env.PINATA_CONTENT;
 const SMART_CONTRACT = process.env.SMART_CONTRACT;
